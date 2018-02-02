23e887fdd13165eed003a6a5dafb05e4229bf6ee
petrofang <gilescooper@gmail.com>
2018-02-02 01:31:23 -0500

    Litson Villas Business Card App, v 0.1

---
+++ app/src/main/java/g/unit/litsonvillas/MainActivity.java
@@ -0,0 +1,13 @@
+package g.unit.litsonvillas;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
